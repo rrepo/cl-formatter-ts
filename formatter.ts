9e1d17aef8f5ea987e77c6c60725061c3cdcd79d
repo <reporepo254@gmail.com>
2024-10-27 18:11:24 +0900

    add

--- formatter.ts
+++ formatter.ts
@@ -55,9 +55,31 @@ function formatLispCode(code: string): string {
 
 // テキストファイルへの出力
 const initcode = `
-(defparameter *nodes* (    (       living-room (you are in the living room. a wizard is snoring loudly on the couch.))(garden (you are in a beautiful garden. there is a well front of you.))(attic (are you in the attic. there is a giant welding torch in the corner.))))(defun describe-location (location nodes) (cadr (assoc location nodes)))(describe-location       'living-room *nodes*)
-(apply #'+ '(1 2 3)) 
-`;
+(setq *print-level* nil)
+(setq *print-length* nil)
+
+(defparameter 
+  *nodes*
+  '((living-room (you are in the living room. a wizard is snoring loudly on the couch.))
+    (garden (you are in a beautiful garden. there is a well front of you.))
+    (attic (are you in the attic. there is a giant welding torch in the corner.))
+   )
+)
+
+(defun describe-location (location nodes) 
+  (cadr (assoc location nodes))
+)
+
+(describe-location 'living-room *nodes*)
+
+(defparameter 
+  *edges*
+  \`
+  ((living-room (garden west door) (attic upstairs ladder)) 
+    (garden (living-room east door))
+    (attic (living-room downstairs ladder))
+  )
+)`;
 const formattedCode = formatLispCode(initcode);
 
 fs.writeFileSync('formatted_lisp_code.txt', formattedCode);
