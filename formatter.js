b7f26ae2fd8b0421a988292c59b4f7aa950ad2b3
rrepo <reporepo254@gmail.com>
2024-11-21 00:33:58 +0900

    update comment

--- formatter.js
+++ formatter.js
@@ -45,12 +45,36 @@ function formatLispCode(code) {
         else if (char === ')') {
             indentLevel = Math.max(0, indentLevel - 1);
             result += ")";
+            // }
+            // else if (char == ";" && code[i + 1] == ";") {
+            //   result += `\n${' '.repeat(indentLevel * 2)};`;
         }
-        else if (char == ";" && code[i + 1] == ";") {
-            result += "\n".concat(' '.repeat(indentLevel * 2), ";");
+        else if (char == ";") {
+            if (code[i + 1] == ";") {
+                console.log(code[i + 1]);
+                result += "\n";
+            }
+            // `;` が出現した場合
+            result += char; // 現在の `char` を追加
+            // 次の文字を調べる
+            while (i + 1 < code.length) {
+                var nextChar = code[i + 1];
+                if (nextChar === "T" && code[i + 2] === "T" && code[i + 3] === "T") {
+                    // `TTT` を検出した場合
+                    i += 3; // `TTT` をスキップ
+                    result += "\n"; // 改行を追加
+                    break; // ループを抜ける
+                }
+                else {
+                    // `TTT` に到達するまで文字を追加
+                    result += nextChar;
+                    i++; // ポインタを進める
+                }
+            }
         }
         else if (char == "T" && code[i + 1] == "T" && code[i + 2] == "T") {
             i = i + 3;
+            // result += "\n"
             if (code[i + 1] != "(") {
                 result += "\n".concat(' '.repeat(indentLevel * 2));
             }
@@ -66,7 +90,7 @@ function formatLispCode(code) {
     // .join('\n');
 }
 // テキストファイルへの出力
-var initcode = "\n(defparameter \n  *edges*\n  `\n((living-room (garden west door) (attic upstairs ;;trtttere\n ladder))\n(garden (living-room east door)\n);; test\n(attic (living-room downstairs ;test\nladder))\n))";
+var initcode = "\n(defparameter \n  *edges*\n  `\n((living-room (garden west door) (attic upstairs ;;trtttere\n ladder))\n(garden (living-room east door)\n);; test\n(attic (living-room downstairs ;test\nladder))\n))\n\n; testts\n; (gdgda\n";
 var formattedCode = formatLispCode(initcode);
 fs.writeFileSync('formatted_lisp_code.txt', formattedCode);
 console.log("フォーマット済みのコードが 'formatted_lisp_code.txt' に出力されました。");
